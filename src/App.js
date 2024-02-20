07a3b3a2ad569731a8ff6438d150076fd1bd9a49
Felipe <fpizarro.ux@gmail.com>
2024-02-20 21:01:52 +0100

    posts aggiornato

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './components/NavbarComponent';
 import PostsComponent from './components/PostsComponent';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <PostsComponent/>
+      
     </div>
   );
 }
